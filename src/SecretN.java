f99df48d1e2fe83e856b7c5ed01050d912ed9c6b
JvTolentinoS <73199533+JvTolentinoS@users.noreply.github.com>
2024-08-26 23:51:39 -0300

    Add files via upload
    v2 final

--- src/SecretN.java
+++ src/SecretN.java
@@ -4,32 +4,84 @@ import java.util.Scanner;
 public class SecretN {
 
     public static void main(String[] args) {
-        Random gerador = new Random();
-        int numeroaleatorio = gerador.nextInt(10);
-        int numero = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println(numeroaleatorio);
-        System.out.println("Escreva seu nome");
-        String nome = scanner.nextLine();
+        Random gerador = new Random();
+        boolean continuar = true;
 
-        int numerocorreto = 0;
+            System.out.println("  /$$$$$$                                            /$$           /$$   /$$");
+            System.out.println(" /$$__  $$                                          | $$          | $$$ | $$");
+            System.out.println("| $$  \\__/  /$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$  /$$$$$$        | $$$$| $$");
+            System.out.println("|  $$$$$$  /$$__  $$ /$$_____/ /$$__  $$ /$$__  $$|_  $$_/        | $$ $$ $$");
+            System.out.println(" \\____  $$| $$$$$$$$| $$      | $$  \\__/| $$$$$$$$  | $$          | $$  $$$$");
+            System.out.println(" /$$  \\ $$| $$_____/| $$      | $$      | $$_____/  | $$ /$$      | $$\\  $$$");
+            System.out.println("|  $$$$$$/|  $$$$$$$|  $$$$$$$| $$      |  $$$$$$$  |  $$$$/      | $$ \\  $$");
+            System.out.println(" \\______/  \\_______/ \\_______/|__/       \\_______/   \\___/        |__/  \\__/");
+            System.out.println("                                                                            ");
+            System.out.println("                                                                            ");
 
-        while (numeroaleatorio != numerocorreto) {
+        while (continuar) {
+            int numeroAleatorio = gerador.nextInt(10);
+            int numero = -1;
             int tentativas = 0;
+
+            System.out.println("Escreva seu nome:");
+            String nome = scanner.nextLine();
+
             for (int i = 0; i < 10; i++) {
                 System.out.println("Qual é o número da sorte?");
                 numero = scanner.nextInt();
-                if (numero > numeroaleatorio) {
-                    System.out.println("Acima");
-                } else if (numero < numeroaleatorio) {
-                    System.out.println("Abaixo");
+                tentativas++;
+
+                if (numero > numeroAleatorio) {
+                    System.out.println("Acima demais!");
+                } else if (numero < numeroAleatorio) {
+                    System.out.println("Baixo demais!");
                 } else {
-                    System.out.println("Parabéns " + nome);
+                    System.out.println("Parabéns " + nome + " - O número da sorte é: " + numero +
+                            ", você precisou de " + tentativas + " tentativas.");
+                    break;
                 }
-                numerocorreto = numero;
+            }
+
+            if (numero != numeroAleatorio) {
+                System.out.println(" ");
+                System.out.println(" ");
+                System.out.println(" /$$$$$$$$             /$$$");
+                System.out.println("| $$_____/            /$$_/");
+                System.out.println("| $$             /$$ /$$/  ");
+                System.out.println("| $$$$$         |__/| $$   ");
+                System.out.println("| $$__/             | $$   ");
+                System.out.println("| $$             /$$|  $$  ");
+                System.out.println("| $$            |__/ \\  $$$");
+                System.out.println("|__/                  \\___/");
+                System.out.println(" ");
+                System.out.println(" ");
+                System.out.println("Você falhou! O número correto era: " + numeroAleatorio);
+            }
+
+            System.out.println("Deseja jogar novamente? (s/n)");
+            scanner.nextLine();
+            String resposta = scanner.nextLine();
+
+            if (!resposta.equalsIgnoreCase("s")) {
+                continuar = false;
             }
         }
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("  /$$$$$$  /$$                 /$$                           /$$           /$$");
+        System.out.println(" /$$__  $$| $$                |__/                          | $$          | $$");
+        System.out.println("| $$  \\ $$| $$$$$$$   /$$$$$$  /$$  /$$$$$$   /$$$$$$   /$$$$$$$  /$$$$$$ | $$");
+        System.out.println("| $$  | $$| $$__  $$ /$$__  $$| $$ /$$__  $$ |____  $$ /$$__  $$ /$$__  $$| $$");
+        System.out.println("| $$  | $$| $$  \\ $$| $$  \\__/| $$| $$  \\ $$  /$$$$$$$| $$  | $$| $$  \\ $$|__/");
+        System.out.println("| $$  | $$| $$  | $$| $$      | $$| $$  | $$ /$$__  $$| $$  | $$| $$  | $$    ");
+        System.out.println("|  $$$$$$/| $$$$$$$/| $$      | $$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$/ /$$");
+        System.out.println(" \\______/ |_______/ |__/      |__/ \\____  $$ \\_______/ \\_______/ \\______/ |__/");
+        System.out.println("                                   /$$  \\ $$                                  ");
+        System.out.println("                                  |  $$$$$$/                                  ");
+        System.out.println("                                   \\______/                                   ");
+
+        scanner.close();
     }
 }
-
-
